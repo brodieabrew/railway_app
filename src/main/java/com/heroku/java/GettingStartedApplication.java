c49460f6ec779cf02e49b3e7cff3fd285b26ad30
Brodie Abrew <brodieabrew@u.boisestate.edu>
2025-03-24 10:36:42 -0600

    Add random strings

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -10,11 +10,25 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 @SpringBootApplication
 @Controller
 public class GettingStartedApplication {
     private final DataSource dataSource;
+
+    // Credits: https://stackoverflow.com/a/20536597
+    private String getRandomString() {
+        String SALTCHARS = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder salt = new StringBuilder();
+        Random random = new Random();
+        while (salt.length() < 10) {
+            int index = (int) (random.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+
+        return salt.toString();
+    }
 
     @Autowired
     public GettingStartedApplication(DataSource dataSource) {
@@ -30,13 +44,16 @@ public class GettingStartedApplication {
     String database(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             final var statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+            statement.executeUpdate(
+                    "CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
+            statement.executeUpdate(
+                    "INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
 
-            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
+            final var resultSet = statement.executeQuery("SELECT * FROM table_timestamp_and_random_string");
             final var output = new ArrayList<>();
             while (resultSet.next()) {
-                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+                output.add(
+                        "Read from DB: " + resultSet.getTimestamp("tick") + " " + resultSet.getString("random_string"));
             }
 
             model.put("records", output);
